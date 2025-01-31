a60aadb4717a343354c158c6b18411ffdeb37892
Deepta Roy <116867420+ROYMYSTERIO619@users.noreply.github.com>
2025-01-31 23:33:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Solve the Sudoku/solve-the-sudoku.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(vector<vector<int>>& board, int row, int col, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num || 
+                board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num)
+                return false;
+        }
+        return true;
+    }
+    
+    bool solve(vector<vector<int>>& board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) return true;
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    void solveSudoku(vector<vector<int>>& mat) {
+        solve(mat);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+vector<int> inputLine() {
+    string str;
+    getline(cin, str);
+    stringstream ss(str);
+    int num;
+    vector<int> res;
+    while (ss >> num) {
+        res.push_back(num);
+    }
+    return res;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<vector<int>> grid;
+        for (int i = 0; i < 9; i++) {
+            vector<int> v = inputLine();
+            grid.push_back(v);
+        }
+
+        Solution ob;
+
+        ob.solveSudoku(grid);
+
+        for (auto v : grid) {
+            for (auto elem : v) {
+                cout << elem << " ";
+            }
+            cout << endl;
+        }
+
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
