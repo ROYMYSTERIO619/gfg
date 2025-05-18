1be0cf138e502853a85adbe4124fc93fdd6ab3f5
Deepta Roy <116867420+ROYMYSTERIO619@users.noreply.github.com>
2025-05-18 23:34:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Level Order in spiral form/level-order-in-spiral-form.cpp
@@ -0,0 +1,157 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    return new Node(val);
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input string after splitting by space
+    vector<string> ip;
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child
+class Node
+{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+class Solution {
+  public:
+    vector<int> findSpiral(Node* root) {
+        if (!root) return {};
+
+        vector<int> result;
+        stack<Node*> s1; // For levels to be printed right to left
+        stack<Node*> s2; // For levels to be printed left to right
+
+        s1.push(root);
+
+        while (!s1.empty() || !s2.empty()) {
+            while (!s1.empty()) {
+                Node* node = s1.top(); s1.pop();
+                result.push_back(node->data);
+
+                // Push children in reverse order for next level
+                if (node->right) s2.push(node->right);
+                if (node->left) s2.push(node->left);
+            }
+
+            while (!s2.empty()) {
+                Node* node = s2.top(); s2.pop();
+                result.push_back(node->data);
+
+                // Push children in normal order for next level
+                if (node->left) s1.push(node->left);
+                if (node->right) s1.push(node->right);
+            }
+        }
+
+        return result;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    Solution sol; // Create Solution object
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        // Call the findSpiral function from Solution class
+        vector<int> vec = sol.findSpiral(root);
+        for (int x : vec)
+            cout << x << " ";
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
